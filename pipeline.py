e8edfe5228fdb5558ea11a2c4701121eb5de9a82
Daniel <dan3el@o2.pl>
2024-12-21 13:53:50 +0100

    pipe bq>storage

--- pipeline.py
+++ pipeline.py
@@ -97,30 +97,30 @@ def format_collisions_csv(record):
     return f"{safe_str(record['mmsi_a'])},{safe_str(record['mmsi_b'])},{safe_str(record['timestamp'])},{safe_str(record['cpa'])},{safe_str(record['tcpa'])},{safe_str(record['latitude_a'])},{safe_str(record['longitude_a'])},{safe_str(record['latitude_b'])},{safe_str(record['longitude_b'])}"
 
 def run():
-    project_id=os.getenv('GOOGLE_CLOUD_PROJECT')
+    project_id = os.getenv('GOOGLE_CLOUD_PROJECT')
     if not project_id:
         raise ValueError("GOOGLE_CLOUD_PROJECT not set")
 
-    pipeline_options=PipelineOptions()
-    pipeline_options.view_as(StandardOptions).streaming=True
-    gcloud_options=pipeline_options.view_as(GoogleCloudOptions)
-    gcloud_options.project=project_id
-    gcloud_options.region=os.getenv('REGION','us-east1')
+    pipeline_options = PipelineOptions()
+    pipeline_options.view_as(StandardOptions).streaming = True
+    gcloud_options = pipeline_options.view_as(GoogleCloudOptions)
+    gcloud_options.project = project_id
+    gcloud_options.region = os.getenv('REGION','us-east1')
 
-    input_subscription=pipeline_options.view_as(MyPipelineOptions).input_subscription
+    input_subscription = pipeline_options.view_as(MyPipelineOptions).input_subscription
 
     with beam.Pipeline(options=pipeline_options) as p:
-        parsed=(
+        parsed = (
             p
             | 'ReadFromPubSub' >> beam.io.ReadFromPubSub(subscription=input_subscription)
             | 'ParseMessage' >> beam.Map(parse_message_with_timestamp)
-            | 'FilterNone' >> beam.Filter(lambda x: x[0] is not None)  # x=(record, event_ts)
+            | 'FilterNone' >> beam.Filter(lambda x: x[0] is not None)
             | 'AssignTimestamps' >> beam.Map(to_timestamped_value)
             | 'FilterStamp' >> beam.Filter(lambda x: x is not None)
         )
 
         # Ships
-        ships_windowed=(
+        ships_windowed = (
             parsed
             | 'WindowForShips' >> beam.WindowInto(
                 FixedWindows(60),
@@ -132,14 +132,12 @@ def run():
         (ships_windowed
          | 'FormatShipsCSV' >> beam.Map(format_ships_csv)
          | 'WriteShipsToGCS' >> beam.io.WriteToText(
-              file_path_prefix='gs://ais-collision-detection-bucket/ais_data/ships/ships',
-              file_name_suffix='.csv',
-              shard_name_template='-SSSS-of-NNNN'
-         )
-        )
+             file_path_prefix='gs://ais-collision-detection-bucket/ais_data/ships/ships',
+             file_name_suffix='.csv',
+             shard_name_template='-SSSS-of-NNNN'))
 
         # Collisions
-        collisions_windowed=(
+        collisions_windowed = (
             parsed
             | 'WindowForCollisions' >> beam.WindowInto(
                 FixedWindows(60),
@@ -154,11 +152,9 @@ def run():
         (collisions_windowed
          | 'FormatCollisionsCSV' >> beam.Map(format_collisions_csv)
          | 'WriteCollisionsToGCS' >> beam.io.WriteToText(
-              file_path_prefix='gs://ais-collision-detection-bucket/ais_data/collisions/collisions',
-              file_name_suffix='.csv',
-              shard_name_template='-SSSS-of-NNNN'
-         )
-        )
+             file_path_prefix='gs://ais-collision-detection-bucket/ais_data/collisions/collisions',
+             file_name_suffix='.csv',
+             shard_name_template='-SSSS-of-NNNN'))
 
-if __name__=='__main__':
+if __name__ == '__main__':
     run()
