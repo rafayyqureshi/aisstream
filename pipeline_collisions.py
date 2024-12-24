88d87b02b8e74e76d0988a86ff206bc88441bda5
Daniel <dan3el@o2.pl>
2024-12-24 09:49:09 +0100

    pipe x2 fix

--- pipeline_collisions.py
+++ pipeline_collisions.py
@@ -1,19 +1,21 @@
 import os
 import json
 import math
+import time  # Do obsługi retencji
 import apache_beam as beam
 from apache_beam.options.pipeline_options import PipelineOptions, StandardOptions
-from apache_beam.transforms.userstate import BagStateSpec, on_timer, TimerSpec, TimeDomain
+from apache_beam.transforms.userstate import BagStateSpec
+from apache_beam.transforms.userstate import on_timer, TimerSpec, TimeDomain
 
 # Parametry wykrywania kolizji
-CPA_THRESHOLD = 0.5     # mile morskie
-TCPA_THRESHOLD = 10.0   # minuty
-STATE_RETENTION_SEC = 30 # jak długo trzymamy stare rekordy w pamięci (np. 30 sek)
+CPA_THRESHOLD = 0.5       # mile morskie
+TCPA_THRESHOLD = 10.0     # minuty
+STATE_RETENTION_SEC = 30  # jak długo trzymamy stare rekordy (w sek.)
 
 def parse_message(record):
     """
-    Prosta funkcja parsująca wiadomość AIS z Pub/Sub.
-    Zwraca None, jeśli nie spełnia minimalnych wymagań.
+    Odczyt i wstępna weryfikacja wiadomości AIS (JSON).
+    Zwraca None, jeśli brakuje kluczowych pól.
     """
     try:
         data = json.loads(record.decode('utf-8'))
@@ -26,16 +28,9 @@ def parse_message(record):
 
 def compute_cpa_tcpa(ship_a, ship_b):
     """
-    Najlepszy (bardziej rozbudowany) algorytm obliczania CPA/TCPA,
-    wykorzystujący skalowanie współrzędnych i prędkości.
-    
-    Założenia:
-      - SOG w węzłach (nm/h).
-      - COG w stopniach (0..359).
-      - latitude, longitude w stopniach.
-      - Brak kolizji, jeśli któryś statek <50m albo brak ship_length.
+    Oblicza CPA/TCPA między dwoma statkami (większy fragment kodu z float konwersją).
     """
-       # Bezpieczna konwersja
+    # Bezpieczna konwersja length
     sl_a = ship_a.get('ship_length')
     sl_b = ship_b.get('ship_length')
     try:
@@ -46,7 +41,7 @@ def compute_cpa_tcpa(ship_a, ship_b):
         sl_b = float(sl_b)
     except:
         return (9999, -1)
-    
+
     # Odrzucamy z obliczeń, jeśli statek <50m
     if sl_a < 50 or sl_b < 50:
         return (9999, -1)
@@ -54,7 +49,7 @@ def compute_cpa_tcpa(ship_a, ship_b):
     # Współczynnik geograficzny
     latRef = (ship_a['latitude'] + ship_b['latitude']) / 2.0
     scaleLat = 111000.0
-    scaleLon = 111000.0 * math.cos(math.radians(latRef))  # w przybliżeniu
+    scaleLon = 111000.0 * math.cos(math.radians(latRef))
 
     def to_xy(lat, lon):
         return (lon * scaleLon, lat * scaleLat)
@@ -62,15 +57,12 @@ def compute_cpa_tcpa(ship_a, ship_b):
     xA, yA = to_xy(ship_a['latitude'], ship_a['longitude'])
     xB, yB = to_xy(ship_b['latitude'], ship_b['longitude'])
 
-    # SOG w węzłach (nm/h).
-    # Do obliczeń w metrach/min trzeba przeskalować.
     sogA = ship_a['sog']  # nm/h
     sogB = ship_b['sog']
 
     def cog_to_vector(cog_deg, sog_nm_h):
         cog_rad = math.radians(cog_deg)
-        # W standardzie AIS, COG=0 oznacza "północ", rośnie w prawo (kompas).
-        # Przyjmujemy vx= sog*sin(cog), vy= sog*cos(cog)
+        # AIS standard: COG=0 => północ
         vx = sog_nm_h * math.sin(cog_rad)
         vy = sog_nm_h * math.cos(cog_rad)
         return vx, vy
@@ -78,33 +70,27 @@ def compute_cpa_tcpa(ship_a, ship_b):
     vxA_nm_per_h, vyA_nm_per_h = cog_to_vector(ship_a['cog'], sogA)
     vxB_nm_per_h, vyB_nm_per_h = cog_to_vector(ship_b['cog'], sogB)
 
-    # Pozycje względne
     dx = xA - xB
     dy = yA - yB
     dvx_nm_per_h = vxA_nm_per_h - vxB_nm_per_h
     dvy_nm_per_h = vyA_nm_per_h - vyB_nm_per_h
 
-    # Przeliczamy nm/h na m/min
-    # 1 nm = 1852 m, nm/h = (1852/60) m/min
+    # nm/h => m/min
     speed_scale = 1852.0 / 60.0
     dvx_m_per_min = dvx_nm_per_h * speed_scale
     dvy_m_per_min = dvy_nm_per_h * speed_scale
 
-    # Obliczenie VV i PV
-    VV_m = dvx_m_per_min ** 2 + dvy_m_per_min ** 2  # prędkość względna ^2
+    VV_m = dvx_m_per_min**2 + dvy_m_per_min**2
     PV_m = dx * dvx_m_per_min + dy * dvy_m_per_min
 
     if VV_m == 0:
-        # statki płyną identycznie
         tcpa = 0.0
     else:
         tcpa = - PV_m / VV_m
 
-    # Jeśli TCPA < 0, kolizja wystąpiła w przeszłości
     if tcpa < 0:
         return (9999, -1)
 
-    # Po tcpa minutach
     vxA_m_per_min = vxA_nm_per_h * speed_scale
     vyA_m_per_min = vyA_nm_per_h * speed_scale
     vxB_m_per_min = vxB_nm_per_h * speed_scale
@@ -115,33 +101,45 @@ def compute_cpa_tcpa(ship_a, ship_b):
     xB2 = xB + vxB_m_per_min * tcpa
     yB2 = yB + vyB_m_per_min * tcpa
 
-    dist_m = math.sqrt((xA2 - xB2) ** 2 + (yA2 - yB2) ** 2)
+    dist_m = math.sqrt((xA2 - xB2)**2 + (yA2 - yB2)**2)
     dist_nm = dist_m / 1852.0
 
     return (dist_nm, tcpa)
 
 class CollisionDoFn(beam.DoFn):
     """
-    Trzyma poprzednie rekordy w stanie (ok. 30 sek),
-    porównuje z nowym, oblicza cpa, tcpa => jeśli cpa < 0.5 nm i tcpa < 10 min => kolizja.
+    Zamiast timerów set_relative etc., przechowujemy w BagState:
+    (ship_record, insertion_time), i w każdej process() usuwamy rekordy starsze niż 30 sek.
     """
     RECORDS_STATE = BagStateSpec('records_state', beam.coders.FastPrimitivesCoder())
-    TIMER = TimerSpec('cleanup', TimeDomain.WATERMARK)
-    
 
-    def process(self, element, records_state=beam.DoFn.StateParam(RECORDS_STATE),
-                timer=beam.DoFn.TimerParam(TIMER)):
+    def process(self, element, records_state=beam.DoFn.StateParam(RECORDS_STATE)):
         geohash, ship = element
+        now_sec = time.time()
 
-        # Dodaj bieżący record do stanu
-        records_state.add(ship)
+        # Dodaj (ship, now_sec) do stanu
+        records_state.add((ship, now_sec))
 
-        # Ustaw timer, by za 30 sek (processing-time) wywołać cleanup
-        timer.set_relative(STATE_RETENTION_SEC)
+        # Pobierz wszystko
+        old_records = list(records_state.read())
 
-        # Sprawdź kolizje z poprzednimi
-        old_ships = list(records_state.read())
-        for old_ship in old_ships:
+        fresh_records = []
+        # Usuwamy te, które są starsze niż 30 sek
+        for (s, t) in old_records:
+            if now_sec - t <= STATE_RETENTION_SEC:
+                fresh_records.append((s, t))
+
+        # wyczyść i wstaw tylko świeże
+        records_state.clear()
+        for x in fresh_records:
+            records_state.add(x)
+
+        # teraz collision-check:
+        # (a) obecny rekord to 'ship'
+        # (b) porównuj z 's' z fresh_records
+        for (old_ship, _) in fresh_records:
+            if old_ship is ship:
+                continue
             cpa, tcpa = compute_cpa_tcpa(old_ship, ship)
             if cpa < CPA_THRESHOLD and tcpa >= 0 and tcpa < TCPA_THRESHOLD:
                 yield {
@@ -155,13 +153,6 @@ class CollisionDoFn(beam.DoFn):
                     'latitude_b': ship['latitude'],
                     'longitude_b': ship['longitude']
                 }
-
-        # Ustaw timer cleanup
-        timer.set(timer.now() + STATE_RETENTION_SEC)
-
-    @on_timer(TIMER)
-    def cleanup(self, records_state=beam.DoFn.StateParam(RECORDS_STATE)):
-        records_state.clear()
 
 class MyPipelineOptions(PipelineOptions):
     @classmethod
@@ -181,26 +172,19 @@ def run():
             p
             | 'ReadPubSub' >> beam.io.ReadFromPubSub(subscription=input_subscription)
         )
-
         parsed = (
             lines
             | 'ParseAIS' >> beam.Map(parse_message)
             | 'FilterNone' >> beam.Filter(lambda x: x is not None)
         )
-
-        # Key by geohash – klucz do stateful processing
         keyed = (
             parsed
-            | 'KeyByGeohash' >> beam.Map(lambda r: (r.get('geohash','none'), r))
+            | 'KeyByGeohash' >> beam.Map(lambda r: (r.get('geohash', 'none'), r))
         )
-
-        # Wykrywanie kolizji z StatefulDoFn
         collisions = (
             keyed
             | 'DetectCollisions' >> beam.ParDo(CollisionDoFn())
         )
-
-        # Publikowanie do Pub/Sub collisions_topic
         collisions_str = (
             collisions
             | 'CollisionsToJson' >> beam.Map(lambda x: json.dumps(x).encode('utf-8'))
