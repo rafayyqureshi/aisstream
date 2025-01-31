61d811e99556718a5369f5d8c0d5d92f118695e2
Daniel <dan3el@o2.pl>
2025-01-31 06:50:56 +0100

    pipe + coll

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
-# setup.py
-
 import setuptools
 
 setuptools.setup(
     name='ais_project',
     version='1.0',
-    py_modules=['cpa_utils'],
+    py_modules=[
+        'cpa_utils',        # plik cpa_utils.py
+        'collision_dofn',   # plik collision_dofn.py
+    ],
 )
